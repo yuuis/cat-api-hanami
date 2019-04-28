b61b5dd0bf6acfd4bd77c028d555cae8662c07cf
Yu Ishiguro <yuuis.egr@gmail.com>
2019-04-28 13:00:25 +0900

    remove unuse codes

--- lib/cat_api_hanami/repositories/cat_repository.rb
+++ lib/cat_api_hanami/repositories/cat_repository.rb
@@ -1,24 +1,4 @@
 # frozen_string_literal: true
 
 class CatRepository < Hanami::Repository
-  # def all(_id)
-  #   cats = cats.select(:id, :name)
-  #   cats
-  # rescue StandardError
-  #   raise $ERROR_INFO
-  # end
-
-  # def find(id)
-  #   cat = cats.where(id: id).first
-  #   return cat if cat
-  # rescue StandardError
-  #   raise $ERROR_INFO
-  # end
-
-  # def create(name)
-  #   cat = create(name: name)
-  #   return cat if cat
-  # rescue StandardError
-  #   raise $ERROR_INFO
-  # end
 end
